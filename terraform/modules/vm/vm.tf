98dc4074b5ebdaeb99c8bef8a98396745e5e4afe
Huy Tran Nguyen Quang <huy.tran4997@gmail.com>
2022-08-04 08:22:31 +0700

    first commnit

---
+++ terraform/modules/vm/vm.tf
@@ -0,0 +1,37 @@
+resource "azurerm_network_interface" "network" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${var.public_ip}"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  size                = "Standard_B1s"
+  admin_username                  = "ngvbach2000"
+  network_interface_ids = [
+    azurerm_network_interface.network.id,
+  ]
+  admin_ssh_key {
+    username = "ngvbach2000"
+    public_key = file(".ssh/id_rsa.pub")
+  }
+  os_disk {
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal-daily"
+    sku       = "20_04-daily-lts-gen2"
+    version   = "latest"
+  }
+}
